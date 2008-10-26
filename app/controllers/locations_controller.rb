e054c5297c397513ecf5c1e514076d0a432702e5
Chris78 <cgerharz@.(none)>
2008-10-26 17:34:34 +0100

    * filter files by location, path and filename and tag the results * finally some more beautiful styles * some tooltips and icons instead of '[X]'

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,6 +1,8 @@
 class LocationsController < ApplicationController
+  before_filter :set_menu
   def index
     @locations=Location.find(:all)
+    select_menu('manage')
   end
   def create
     params[:location][:type].constantize.create(params[:location])
@@ -9,5 +11,34 @@ class LocationsController < ApplicationController
   def delete
     Location.delete(params[:id])
     redirect_to :action=>:index
+  end
+
+  def browse
+    select_menu('browse')
+    @location=Location.find(params[:location_id]) unless params[:location_id].blank?
+    @locations=Location.all(:conditions=>"type NOT IN ('Computer','HddCase')")
+    conds=[]
+    conds += params[:filter_filename].split(/\s+/).map{|x| "file_locations.filename LIKE \"%#{x}%\""} unless params[:filter_filename].blank?
+    conds += params[:filter_path].split(/\s+/).map{|x| "file_locations.path LIKE \"%#{x}%\""} unless params[:filter_path].blank?
+    conds << "file_locations.location_id=#{@location.id}" if @location
+    unless conds.blank?
+      @fileinfos=Fileinfo.paginate(:include=>:file_locations, :conditions=>conds.join(' AND '), :per_page=>params[:per_page]||10, :page=>params[:page])
+    end
+  end
+
+  private
+
+  def set_menu
+    menu_add('Locations verwalten', '/locations', :id=>'manage')
+    menu_add('Locations durchsuchen', '/locations/browse', :id=>'browse')
+    #menu_add('Location-Details bearbeiten', '/locations/edit', :id=>'edit')
+    menu_add('Location-Details bearbeiten', '#', :onclick=>"alert('Kommt auch bald...'); return false", :id=>'edit')
+  end
+  def menu_add(name,url,options={})
+    @menu||=[]
+    @menu<<{:name=>name, :url=>url, :html_options=>options}
+  end
+  def select_menu(id)
+    @selected=id
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,14 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   include TagsHelper
+
+  def location_selector(locations,options={})
+    include_blank=options[:include_blank]||false
+    render :partial=>'shared/select_location', :locals=>options.update(:locations=>locations, :include_blank=>include_blank)
+  end
+
+  def icon(name)
+    image_tag('/images/'+name+'.gif', :border=>:none)
+  end
+
 end
