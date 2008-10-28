503a05b947d76c58dc0c67723ff5f33aa99f740d
unknown <Gerry@.(none)>
2008-10-28 20:16:42 +0100

    tag cloud with limited tags; some experimental stuff on the form

--- Unit2.dfm
+++ Unit2.dfm
@@ -1,8 +1,8 @@
 object Form1: TForm1
-  Left = 247
-  Top = 166
-  Width = 1142
-  Height = 656
+  Left = 41
+  Top = 141
+  Width = 1567
+  Height = 1058
   Caption = 'Tgit GUI'
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
@@ -13,6 +13,9 @@ object Form1: TForm1
   OldCreateOrder = False
   OnCreate = FormCreate
   OnDestroy = FormDestroy
+  DesignSize = (
+    1559
+    1031)
   PixelsPerInch = 96
   TextHeight = 13
   object Label1: TLabel
@@ -56,10 +59,31 @@ object Form1: TForm1
   object GroupBox1: TGroupBox
     Left = 744
     Top = 16
-    Width = 369
-    Height = 297
+    Width = 794
+    Height = 699
+    Anchors = [akLeft, akTop, akRight, akBottom]
     Caption = 'GroupBox1'
     TabOrder = 3
+    object Label2: TLabel
+      Left = 2
+      Top = 28
+      Width = 32
+      Height = 669
+      Align = alLeft
+      BiDiMode = bdLeftToRight
+      Caption = 'Label2'
+      ParentBiDiMode = False
+    end
+    object Label3: TLabel
+      Left = 2
+      Top = 15
+      Width = 790
+      Height = 13
+      Align = alTop
+      BiDiMode = bdLeftToRight
+      Caption = 'Label3'
+      ParentBiDiMode = False
+    end
   end
   object CheckBox1: TCheckBox
     Left = 496
@@ -72,12 +96,37 @@ object Form1: TForm1
     TabOrder = 4
   end
   object Button1: TButton
-    Left = 104
-    Top = 40
+    Left = 40
+    Top = 32
     Width = 75
     Height = 25
-    Caption = 'Test DLL'
+    Caption = 'Test SHA2'
     TabOrder = 5
     OnClick = Button1Click
+  end
+  object TabbedNotebook1: TTabbedNotebook
+    Left = 16
+    Top = 16
+    Width = 705
+    Height = 577
+    TabsPerRow = 15
+    TabFont.Charset = DEFAULT_CHARSET
+    TabFont.Color = clBtnText
+    TabFont.Height = -11
+    TabFont.Name = 'MS Sans Serif'
+    TabFont.Style = []
+    TabOrder = 6
+    object TTabPage
+      Left = 4
+      Top = 24
+      Caption = 'Default'
+    end
+  end
+  object DCP_sha256: TDCP_sha256
+    Id = 28
+    Algorithm = 'SHA256'
+    HashSize = 256
+    Left = 8
+    Top = 32
   end
 end
--- Unit2.pas
+++ Unit2.pas
@@ -4,9 +4,11 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, StdCtrls, SQLiteTable3, ComCtrls, Grids, StringItWell;
+  Dialogs, StdCtrls, SQLiteTable3, ComCtrls, Grids, StringItWell,
+  DCPcrypt2, DCPsha256, Math, TabNotBk;
 
 type
+  TByteArray = Array of Byte;
   TForm1 = class(TForm)
     StringGrid1: TStringGrid;
     edtSelectedTags: TEdit;
@@ -15,13 +17,14 @@ type
     CheckBox1: TCheckBox;
     Label1: TLabel;
     Button1: TButton;
+    DCP_sha256: TDCP_sha256;
+    Label2: TLabel;
+    Label3: TLabel;
+    TabbedNotebook1: TTabbedNotebook;
+    
     procedure FormCreate(Sender: TObject);
     procedure FormDestroy(Sender: TObject);
-//    procedure GetTagCloudAll(Sender: TObject);
     procedure StringGrid1SelectCell(Sender: TObject; ACol, ARow: Integer; var CanSelect: Boolean);
-    procedure showFileinfos(sltb: TSQLiteTable);
-    procedure showTagcloud(tags:TSQLIteTable);
-    procedure ReloadTagCloud();
     procedure edtSelectedTagsChange(Sender: TObject);
     procedure Button1Click(Sender: TObject);
   private
@@ -29,16 +32,20 @@ type
     slDBPath: String;
     sldb: TSQLiteDatabase;
     sltb: TSQLiteTable;
-    function GetTagCloudAll(): TSQLIteTable;
-    function GetItemsFor(tags: TStringList; match_all:Boolean): TSQLIteTable;
+    procedure ReloadTagCloud(limit:Integer);
+    procedure showFileinfos(sltb: TSQLiteTable);
+    procedure showTagcloud(tags:TSQLIteTable);
+    function  GetTagCloudAll(limit:Integer): TSQLIteTable;
+    function  GetItemsFor(tags: TStringList; match_all:Boolean): TSQLIteTable;
+    procedure renderTag(tag_item,tag_count:String;i:Integer);
+    function  GetSha2(filename:String): String;
+    function  Sha2(s:String): String;
+    function  StringToArrayOfBytes(s:String): TByteArray;
+    procedure alert(s:String);
+
   public
     { Public declarations }
   end;
-
-  const
-  dieDll = 'libeay32.dll';
-  function EVP_sha256(var s : PChar) : String; external dieDll;
-
 
 var
   Form1: TForm1;
@@ -50,9 +57,9 @@ implementation
 procedure TForm1.FormCreate(Sender: TObject);
 var res:TSQLiteTable;
 begin
-  slDBPath := ExtractFilepath(application.exename)+ 'tgit.db';
+  slDBPath := ExtractFilepath(application.exename)+ 'db\tgit.db';
   sldb := TSQLiteDatabase.Create(slDBPath);
-  reloadTagCloud();
+  reloadTagCloud(8);
 end;
 
 function TForm1.GetItemsFor(tags:TStringList; match_all: Boolean):TSQLiteTable;
@@ -95,16 +102,22 @@ begin
   end;
 end;
 
-procedure TForm1.ReloadTagCloud();
+procedure TForm1.ReloadTagCloud(limit:Integer);
 begin
-  showTagcloud(GetTagCloudAll());
+  showTagcloud(GetTagCloudAll(limit));
 end;
 
-function TForm1.GetTagCloudAll(): TSQLIteTable;
+function TForm1.GetTagCloudAll(limit:Integer): TSQLIteTable;
 begin
-  result := slDb.GetTable('SELECT *, count(*) as anz '+
-    'FROM tags LEFT JOIN taggings on taggings.tag_id=tags.id '+
-    'group by tags.id order by anz desc, name');
+  result := slDb.GetTable('SELECT name, id, anz '+
+    'FROM '+
+    '  ( SELECT name, tags.id AS id, count(*) as anz '+
+    '    FROM tags '+
+    '    LEFT JOIN taggings on taggings.tag_id=tags.id '+
+    '    GROUP BY tags.id order by anz DESC'+
+    '    LIMIT '+inttostr(limit)+
+    '  ) AS temp '+
+    'ORDER BY name');
 end;
 
 
@@ -119,22 +132,46 @@ begin
     begin
       tag_item:=UTF8Decode(tags.FieldAsString(tags.FieldIndex['Name']));
       tag_count:=tags.FieldAsString(tags.FieldIndex['anz']);
-      StringGrid1.rowcount:=i+1;
-      StringGrid1.cells[0,i]:=tag_item;
-      StringGrid1.cells[1,i]:=tag_count;
+      renderTag(tag_item,tag_count,i);
       inc(i);
       tags.Next;
     end;
   end;
+  TabbedNotebook1.Pages.Delete(0);
+  TabbedNotebook1.PageIndex:=0;
   tags.Free;
+end;
+
+procedure TForm1.renderTag(tag_item,tag_count:String; i:Integer);
+// i ist die "laufende Nummer" der Tags (wenn diese z.B. in ein Stringgrid eingetragen werden sollen)
+var tag: TLabel;
+j,k:Integer;
+begin
+// Stringgrid-Variante:
+//  StringGrid1.rowcount:=i+1;
+//  StringGrid1.cells[0,i]:=tag_item;
+//  StringGrid1.cells[1,i]:=tag_count;
+  tag:=TLabel.create(GroupBox1);
+  tag.Parent:=GroupBox1;
+  tag.Caption:=tag_item+' ('+tag_count+')';
+  TabbedNotebook1.Pages.Add(tag.Caption);
+  j:=i div 5;
+  k:=i mod 5;
+  tag.Top:=j*tag.Height;
+  tag.Left:=k*tag.Width;
+  //tag.Transparent:=true;
+  tag.align:=alLeft;
+  tag.Font.Size:=min(20,floor(8+strtoint(tag_count)/10));
 end;
 
 procedure TForm1.FormDestroy(Sender: TObject);
 begin
   sltb.free;
   sldb.free;
+  while GroupBox1.ControlCount>0 do
+    GroupBox1.Controls[0].free;
 end;
-          
+
 procedure TForm1.StringGrid1SelectCell(Sender: TObject; ACol,ARow: Integer; var CanSelect: Boolean);
 var a: TStringList;
 var res: TSQLiteTable;
@@ -161,7 +198,6 @@ begin
   showFileinfos(res);
 end;
 
-
 procedure TForm1.edtSelectedTagsChange(Sender: TObject);
 begin
 Label1.Caption:='_'+strip(edtSelectedTags.text)+'_';
@@ -169,11 +205,72 @@ end;
 
 procedure TForm1.Button1Click(Sender: TObject);
 var s:string;
-var p: PChar;
 begin
   s:='asdf';
-  p:=PChar(s);
-  Label1.Caption:='+'+EVP_sha256(p)+'+';
+  Label1.Caption:='+'+Sha2(edtSelectedTags.text)+'+';
+end;
+
+function TForm1.Sha2(s:String): String;
+var
+  HashDigest: array of byte;
+  i, j, read: integer;
+begin
+  result:='';
+  try
+      DCP_sha256.Init;
+      DCP_sha256.UpdateStr(s);
+      SetLength(HashDigest,DCP_sha256.HashSize div 8);
+      DCP_sha256.Final(HashDigest[0]);  // get the output
+      s := '';
+      for j := 0 to Length(HashDigest) - 1 do  // convert it into a hex string
+        s := s + IntToHex(HashDigest[j],2);
+      result:=lowercase(s);  // <--- SQLite3 ist case-sensitive!
+    except
+      MessageDlg('An error occurred while reading the file',mtError,[mbOK],0);
+    end;
+end;
+
+function TForm1.GetSha2(filename:String): String;
+  var
+    strmInput: TFileStream;
+    HashDigest: array of byte;
+    i, j, read: integer;
+    s: string;
+    buffer: array[0..16383] of byte;
+  begin
+    try
+      DCP_sha256.Init;
+      strmInput := TFileStream.Create(filename,fmOpenRead);
+      repeat
+        read := strmInput.Read(buffer,Sizeof(buffer));
+        DCP_sha256.Update(buffer,read);
+      until read <> Sizeof(buffer);
+      strmInput.Free;
+      SetLength(HashDigest,DCP_sha256.HashSize div 8);
+      DCP_sha256.Final(HashDigest[0]);  // get the output
+      s := '';
+      for j := 0 to Length(HashDigest) - 1 do  // convert it into a hex string
+        s := s + IntToHex(HashDigest[j],2);
+      result:=lowercase(s);  // <--- SQLite3 ist case-sensitive!
+    except
+      strmInput.Free;
+      MessageDlg('An error occurred while reading the file',mtError,[mbOK],0);
+  end;
+end;
+
+
+function TForm1.StringToArrayOfBytes(s:String):TByteArray;
+var
+   j: integer;
+begin
+   SetLength(Result, Length(s)) ;
+   for j := 0 to Length(s) - 1 do
+     Result[j] := ord(s[j + 1]) - 48;
+end;
+
+procedure TForm1.alert(s:String);
+begin
+  messageDlg(s,mtInformation,[mbOK],0);
 end;
 
 end.
